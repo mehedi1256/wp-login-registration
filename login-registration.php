f695d0a7959ae8839c1beb2984761c8006ddffc4
mehedi1256 <mehedihassanshovospirit@gmail.com>
2025-02-10 19:14:58 +0600

    dev plugin

---
+++ login-registration.php
@@ -0,0 +1,16 @@
+/*
+ * Plugin Name:       Login and Registration
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Login, Registration with authentication and authorization.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mehedi Hassan Shovo
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       login-and-registration
+ * Domain Path:       /languages
+ * Requires Plugins:  my-plugin, yet-another-plugin
+ */
